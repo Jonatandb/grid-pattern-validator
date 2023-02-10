8fdd1d9979caa64da62e4507059394c57863ff4d
Ezequiel Gonzalez <ezequiel@pow-gaming.com>
2023-02-10 12:52:14 -0300

    match functionality improved 0=>any | -1=>not match

--- GridPattern.ts
+++ GridPattern.ts
@@ -6,6 +6,12 @@ export type Coordinate = [RowPostion, ColumnPosition]
 // override with type number
 type Cell = number
 
+enum PatternCellStates {
+    Match = 1,
+    Any = 0,
+    NotMatch = -1,
+}
+
 export default class GridPattern extends Grid {
 
     match(grid: Matrix): Coordinate[]|false {
@@ -14,15 +20,17 @@ export default class GridPattern extends Grid {
 
         for (let row = 0; row < this.matrix.length; row++) {
             for (let column = 0; column < this.matrix[row].length; column++) {
-                const currentElement = grid[row][column]
-                const cellActivated = this.matrix[row][column]
+                const currentElement: Cell = grid[row][column] as Cell
+                const cellState = this.matrix[row][column]
                 // store the first active element from the pattern
-                if (mainValue==0 && cellActivated) {
+                if (mainValue==0 && cellState) {
                     mainValue = currentElement as Cell
                 }
-                if ((mainValue!=currentElement && cellActivated) || (mainValue==currentElement && !cellActivated)) {
+                if (cellState==PatternCellStates.Match && mainValue!=currentElement) {
                     return false
-                } else if (mainValue==currentElement && cellActivated) {
+                } else if (cellState==PatternCellStates.NotMatch && mainValue==currentElement) {
+                    return false
+                } else if (cellState==PatternCellStates.Match && mainValue==currentElement) {
                     coordinates.push([row, column])
                 }
             }
